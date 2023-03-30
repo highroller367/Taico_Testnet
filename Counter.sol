a873232adfd518a4566fac26265f68ffb4fc1e4b
highroller367 <113785993+highroller367@users.noreply.github.com>
2023-03-30 14:58:12 +0400

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 private number;
+    uint256 public number;
 
     function setNumber(uint256 newNumber) public {
-        require(newNumber != 666);
         number = newNumber;
+    }
+
+    function increment() public {
+        number++;
     }
 }
